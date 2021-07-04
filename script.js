0870f321d3f6fa96242f391d4affaeaf60fa494b
Juan Ignacio Castelli <juanicaste23@gmail.com>
2021-07-04 20:17:41 -0300

    Updating Script.js (Version 3)
    Ahora tiene la función replegarParrafos, que permite replegar los parrafos para que no esten todos abiertos.

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 const questions = document.querySelectorAll('.question-container__question');
 
-console.log(questions); //probando si anda
+const questionsArray = [...questions];
 
-questions.forEach( question => question.addEventListener('click', desplegar));
+console.log(questions);
+console.log(questionsArray);//probando si anda
 
-function desplegar(e) {
+questions.forEach( question => question.addEventListener('click', desplegarParrafos);
 
-    const answer = e.target.nextElementSibling;
-    const arrowIcon = e.target.childNodes[1];
-    
-    if (answer.style.display === 'block') { 
-        e.target.style.color = 'hsl(240, 6%, 50%)';
+function desplegarParrafos(e) {
+
+    let index = questionsArray.findIndex(el => el.innerText == e.target.innerText );    
+    let answer = e.target.nextElementSibling;
+    let arrowIcon = e.target.childNodes[1];
+
+    if (answer.style.display === 'block') 
+    { 
+        e.target.style.fontWeight = '400';
         arrowIcon.classList.remove('arrow-icon--active');
         answer.style.display = 'none';
     } 
     else 
     {
-        e.target.style.color = 'hsl(238, 29%, 16%)';
+        e.target.style.fontWeight = '700';
         arrowIcon.classList.add('arrow-icon--active');
         answer.style.display = 'block';
     };
+
+    replegarParrafos(index); //Con esta funcion repliego los parrafos para que no me queden todos abiertos.
+}
+
+function replegarParrafos(index){
+
+    questionsArray.forEach (question => {
+      
+        if ( (question.style.fontWeight==='700') && (questionsArray.indexOf(question) !== index) ) 
+        {
+            question.style.fontWeight = '400';
+            question.childNodes[1].classList.remove('arrow-icon--active');
+            question.nextElementSibling.style.display='none';
+        };
+    }
     
-};
+    );
+}
